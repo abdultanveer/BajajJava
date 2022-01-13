15341654fadd588012b4be992dd9d8ce7be841c1
abdul <abdul.tanveer@gmail.com>
2022-01-13 14:49:05 +0530

    constructors

--- bajaj/src/basics/Classroom.java
+++ bajaj/src/basics/Classroom.java
@@ -4,8 +4,10 @@ public class Classroom {
 	
 	public static void main(String[] args) {
 		Student  abdul = new Student(); //creating a box in the RAM/memory
+		Student bStudent = new Student(1,"abhinav");
 		Employee someEmployee = new Employee();
-	
+		System.out.println(abdul.id);
+		bStudent.display();
 	}
 
 }
--- bajaj/src/basics/Student.java
+++ bajaj/src/basics/Student.java
@@ -12,5 +12,21 @@ public class Student {
 	String name;
 	boolean isMale;
 	float stipend;
+	
+	Student(){
+		System.out.println("priyansh:-- abdul im about to construct the box");
+		id =0;
+		name = "ansari";
+		
+	}
+	
+	Student(int i, String nam){
+		id = i;
+		name = nam;
+	}
+	
+	void display(){
+		System.out.println("name is"+ name+" stipend is "+stipend);
+	}
 
 }
