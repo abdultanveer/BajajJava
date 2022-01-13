dd97b733e7196e29c323e3d80febeaf6e43a8501
abdul <abdul.tanveer@gmail.com>
2022-01-13 14:23:51 +0530

    initial commit

---
+++ bajaj/src/basics/Classroom.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Classroom {
+	
+	public static void main(String[] args) {
+		Student  abdul = new Student(); //creating a box in the RAM/memory
+		Employee someEmployee = new Employee();
+	
+	}
+
+}
---
+++ bajaj/src/basics/Employee.java
@@ -0,0 +1,33 @@
+package basics;
+
+/**
+ * this is like water bottle
+ * @author Admin
+ *
+ */
+public class Employee {
+	/**
+	 * this is like water
+	 */
+	private String dateOfBirth; //private -- seal of frooti
+
+	//tap
+	private String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	/**
+	 * cap of the bottle
+	 * @param dateOfBirth
+	 */
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	public int getAge() {
+		String age = dateOfBirth + "";
+		return 18;
+	}
+	
+
+}
